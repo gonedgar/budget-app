96cb822082bee398adb45ea18d7dc12344374b5c
gonedgar <gonzaleztorresedgar@gmail.com>
2023-11-09 13:28:09 -0600

    Se añadió la función para imprimir la categoría con su título entre * y cada elemento con su cantidad

--- budget.py
+++ budget.py
@@ -45,3 +45,54 @@ class Category:
             # Se llama el método deposit del objeto al que se le hace la transferencia
             category.deposit(amount, 'Transfer from '+self.category)
             return True
+
+def print_category(Category):
+    dots = 0
+    title = ''
+    total = 0
+    # Se identifica la cantidad de * que van a la izquierda y a la derecha de la categoría
+    dots = int((30 - len(Category.category)) / 2)
+    # Primer loop para los * a la izquierda de la categoría
+    for x in range(dots):
+        title = title + '*'
+    # Se concatena la categoría con los primeros *
+    title = title + Category.category
+    # Segundo loop para concatenar los * de la derecha a la cadena anterior
+    for x in range(dots):
+        title = title + '*'
+    # Si el título es menor a 30, se añade un * al final para que sea 30
+    if len(title) < 30:
+        title = title + '*'
+    # Se imprime el título
+    print(title)
+    # Loop para imprimir cada elemento de la lista
+    for x in Category.ledger:
+        # Se obtiene la longitud de la cantidad junto con el signo (si es negativo) y dos espacios decimales
+        cantidad = len(str("{a:.2f}").format(a = x['amount']))
+        # Se obtiene la longitud de la descripción
+        descripcion = len(x['description'])
+        # Si la descripción es mayor a 23
+        total = total + x['amount']
+        if descripcion > 23:
+            # La descripción se acorta a 23 y se concatena al inicio de la string
+            imprimir = '' + x['description'][:23]
+            # Se determinan y concatenan los espacios que se dejan de acuerdo al amount
+            for y in range((30 - cantidad) - 23):
+                imprimir = imprimir + ' '
+            # Se concatena el amount formateado con dos decimales
+            imprimir = imprimir + str("{a:.2f}").format(a = x['amount'])
+            # Se imprime el amount en consola
+            print(imprimir)
+        # Si la descripción no es mayor a 23
+        else:
+            # Se concatena la descripción tal cual al inicio de la string
+            imprimir = '' + x['description']
+            # Se determinan y concatenan los espacios que se dejan de acuerdo al amount
+            for y in range((30 - cantidad) - descripcion):
+                imprimir = imprimir + ' '
+            # Se concatena el amount formateado con dos decimales
+            imprimir = imprimir+str("{a:.2f}").format(a = x['amount'])
+            # Se imprime el amount en consola
+            print(imprimir)
+    
+    print("Total:{a: .2f}".format(a = total))
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from budget import Category
+from budget import print_category
 
 clothing = Category('Clothing')
-clothing.deposit(17.50, "First clothing deposit")
+clothing.deposit(17.50, "First clothing deposit because I am not poor")
 clothing.withdraw(10.25, 'Bought a shirt')
-print(clothing.category+" balance:",clothing.get_balance())
+clothing.deposit(117.50, "First clothing deposit because I am not poor")
+# print(clothing.category+" balance:",clothing.get_balance())
 
-food = Category('food')
+food = Category('Food')
 food.deposit(1, 'First food deposit')
 clothing.transfer(25, food)
-print(clothing.category+':', clothing.ledger)
-print(food.category+" balance:",food.get_balance())
-print(food.category+':',food.ledger)
+# print(clothing.category+':', clothing.ledger)
+# print(food.category+" balance:",food.get_balance())
+# print(food.category+':',food.ledger)
+
+print_category(clothing)
+print_category(food)
